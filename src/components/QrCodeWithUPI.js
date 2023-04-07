08c95b0d6df2c6c184c4acc62992cf0125b19e1f
rajeevmohan <rajeevmohan@geekyants.com>
2023-04-07 18:28:15 +0530

    adjustment

--- src/components/QrCodeWithUPI.js
+++ src/components/QrCodeWithUPI.js
@@ -5,10 +5,14 @@ function QrCodeWithUPI() {
   return (
     <div className="flex flex-col items-center bg-gradient-to-r from-indigo-800 via-indigo-500 to-indigo-800 text-black py-8">
       <div className="bg-white rounded  px-5 py-4 flex flex-col items-center">
-        <div className="text-2xl font-medium mb-4">
+        <div className=" text-xl text-center md:text-2xl font-medium mb-4">
           Scan the QR code or use UPI ID:
         </div>
-        <img className="w-64 h-64" src={QrCodeImage} alt="QR Code" />
+        <img
+          className="w-52 h-52 md:w-64 md:h-64"
+          src={QrCodeImage}
+          alt="QR Code"
+        />
         <p className="mt-4 text-lg font-bold">meenasharma@ybl</p>
       </div>
     </div>
